0a2a4852e7be27920e6a5b459fbc8466010a18f9
jerald jose <jeraldjose16@gmail.com>
2023-07-28 10:37:42 +0800

    updates

--- search_toearn.py
+++ search_toearn.py
@@ -2,41 +2,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
+from bs4 import BeautifulSoup
 import time
-import subprocess
 import requests
 # execute in terminal"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --remote-debugging-port=9222 /prefetch:5 --flag-switches-begin --enable-features=msEdgeDevToolsWdpRemoteDebugging --flag-switches-end
+url = "https://newsinfo.inquirer.net/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+trending_topics = soup.find_all("div", id="ncg-info")
 options = Options()
-options.use_chromium = True
-# options.add_argument("--remote-debugging-port=9222")
-options.add_experimental_option("debuggerAddress", "localhost:9222")
-words = [
-    "earn money",
-    "online surveys",
-    "work from home",
-    "passive income",
-    "freelancing",
-    "affiliate marketing",
-    "investment opportunities",
-    "money-saving tips",
-    "side hustles",
-    "cashback rewards",
-    "financial independence",
-    "make money online",
-    "money management",
-    "money mindset",
-    "money-making ideas",
-    "entrepreneurship",
-    "financial freedom",
-    "saving strategies",
-    "money goals",
-    "budgeting tips"
-
-]
 driver = webdriver.Edge(options=options)
+# options.use_chromium = True
+options.add_experimental_option("debuggerAddress", "localhost:9222")
 window_handles = driver.window_handles
 driver.switch_to.window(window_handles[0]) # 0 is the first tab, 1 is the second tab
-for i in words:
+print(trending_topics)
+for i in trending_topics:
+    driver.get("https://www.bing.com/search?q="+i.text)
+    time.sleep(5)
+    # clear the search bar
+    txtb_search = driver.find_element(By.ID, 'sb_form_q')
+    txtb_search.clear()
 
   
     
