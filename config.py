d999e51b2b92e62b2648f53777d0760a465b0477
r47dzt3ch <36290699+r47dzt3ch@users.noreply.github.com>
2023-07-28 09:06:56 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-#config for bing credentials
-email = 'jeraldjose16@gmail.com'
-password = 'd74r3j93527$10@16'
-
-#  crediential for melissa 
-melissa_email = 'jeraldjose16@gmail.com'
-melissa_password = ':z-YnUmmK-9RH55'
